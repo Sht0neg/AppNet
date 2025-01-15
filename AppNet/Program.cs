1af3a2e597183f3939adf43bb8c86f45596ec7f1
Admin <aper@gmail.com>
2025-01-15 21:18:11 +0300

    Добавьте файлы проекта.

---
+++ AppNet/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace AppNet
+{
+    internal class Server
+    {
+        TcpListener _listener;
+        Server() {
+            _listener = new TcpListener(IPAddress.Any, 5000);
+        }
+
+        void Start() {
+            _listener.Start();
+            Console.WriteLine("Сервер запущен на порту 5000");
+            while (true)
+            {
+                var client = _listener.AcceptTcpClient();
+                Console.WriteLine("Клиент подключился");
+                Thread thread = new Thread(HandlerSocket);
+                thread.Start(client);
+            }
+        }
+        void HandlerSocket(object socket) {
+            using (var client = (TcpClient)socket) { 
+                var stream = client.GetStream();
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+
+                while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) != 0)
+                {
+                    string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    dynamic responseData = Newtonsoft.Json.JsonConvert.DeserializeObject(message);
+                    Console.WriteLine($"Сообщение получено: {responseData.messages}");
+                    byte[] res = Encoding.UTF8.GetBytes("Сообщение получено");
+                    stream.Write(res, 0, res.Length);
+                    break;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            var server = new Server();
+            server.Start();
+        }
+    }
+}
---
+++ ClientAppNet/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace ClientAppNet
+{
+    internal class Client
+    {
+        static void Main(string[] args)
+        {
+            var requestData = new
+            {
+                model = "openai/gpt-3.5-turbo",
+                messages = "lox",
+                temperature = 0.7,
+                n = 1,
+                max_tokens = 3000
+            };
+            var jsonRequest = Newtonsoft.Json.JsonConvert.SerializeObject(requestData);
+            try {
+                using (var client = new TcpClient("127.0.0.1", 5000)) {
+                    Console.WriteLine("Соединение установлено");
+                    var stream = client.GetStream();
+                    while (true)
+                    {
+                        //Console.WriteLine("напишите сообщение для отправлки или exit для выхода: ");
+                        //string message = Console.ReadLine();
+                        //if (message.ToLower() == "exit") { break; }
+                        byte[] data = Encoding.UTF8.GetBytes(jsonRequest);
+                        stream.Write(data, 0, data.Length);
+
+                        byte[] buffer = new byte[1024];
+                        int countBytes = stream.Read(buffer, 0, buffer.Length);
+                        var res = Encoding.UTF8.GetString(buffer);
+                        Console.WriteLine($"Ответ от сервера {res}");
+                        break;
+                    }
+                }
+            }
+            catch (Exception e) { Console.WriteLine($"Ошибка {e.Message}"); }
+        }
+    }
+}
